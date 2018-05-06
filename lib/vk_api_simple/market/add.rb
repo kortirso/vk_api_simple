d986df21b79c9d72a48abb5c0a189bfd7fc47e7a
kortirso <kortirso@gmail.com>
2018-05-06 11:18:29 +0700

    fix

--- lib/vk_api_simple/market/add.rb
+++ lib/vk_api_simple/market/add.rb
@@ -3,7 +3,7 @@ module VkApiSimple
     # Save uploaded image at server
     module Add
       def add(args = {})
-        response = RestClient.post("#{BASE_URI}save?access_token=#{token}&owner_id=-#{args[:owner_id]}&category_id=#{args[:category_id]}&price=#{args[:price]}&main_photo_id=#{args[:main_photo_id]}&v=#{api_version}", name: args[:name], description: args[:description])
+        response = RestClient.post("#{BASE_URI}add?access_token=#{token}&owner_id=-#{args[:owner_id]}&category_id=#{args[:category_id]}&price=#{args[:price]}&main_photo_id=#{args[:main_photo_id]}&v=#{api_version}", name: args[:name], description: args[:description])
         JSON.parse(response.body)
       end
     end
